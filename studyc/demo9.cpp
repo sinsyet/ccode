47f17d26f8c6143c19fd8d0176843d311a17091e
YGX <sinsyet@163.com>
2017-12-28 01:05:31 +0800

    study

---
+++ studyc/demo9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>  // 这里不能写成 #include <iostream.h>
+using namespace std;	// 这句要添加 
+
+// 这已经是一个c++程序
+main()
+{
+	int i;
+	float f;
+	cin >> i >> f;	// cin是键盘输入流
+	cout << i <<" -- "<<f; 
+} 
